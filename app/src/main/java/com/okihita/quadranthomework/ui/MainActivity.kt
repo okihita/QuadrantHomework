a4703eed64db41d909052d7a4e759faac825cd23
Okihita Sihaloho <okihita@gmail.com>
2022-07-04 15:05:44 +0700

    Added simple UI, networking, and DI libraries

---
+++ app/src/main/java/com/okihita/quadranthomework/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.okihita.quadranthomework.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.okihita.quadranthomework.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
